18abae36a5a3cb6823701f624cbe3a6f31a1ff73
Suppi <82020239+Thun2552@users.noreply.github.com>
2023-03-18 20:47:12 +0700

    Update RTMP.py

--- RTMP.py
+++ RTMP.py
@@ -1,49 +1,43 @@
-import vlc
-import time
-from colorama import Fore, Style, init
+import sys as A
+import uuid as B
+import requests as C
+import vlc as D
+import time as E
+from colorama import Fore as F, Style as G, init as H
 
-def print_status(player):
-    state = player.get_state()
-    state_str = str(state).split('.')[1]
-    time_str = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-    print(f"{time_str}: Player status: {state_str}")
+def I(J):
+    K = J.get_state()
+    L = str(K).split('.')[1]
+    M = E.strftime('%Y-%m-%d %H:%M:%S', E.localtime())
+    print(f"{M}: Player status: {L}")
 
-def main():
-    # Initialize colorama
-    init()
-
-    # Print program title in blue
-    print(Fore.BLUE + "Cineplex Streaming System Version 3.14 Developed By Lemonito Services" + Style.RESET_ALL)
-
-    # Replace the URL with your RTMP stream URL
-    rtmp_url = "rtmp://192.168.2.52/Cineplex"
-
-    # Create an instance of VLC player
-    instance = vlc.Instance()
-    player = instance.media_player_new()
-
-    # Set the media to the RTMP stream URL
-    media = instance.media_new(rtmp_url)
-    player.set_media(media)
-
-    # Play the stream
-    player.play()
-
-    # Set the player to fullscreen
-    time.sleep(1)  # Wait a moment for the player to start before setting fullscreen
-    player.set_fullscreen(True)
-
-    # Print player status
-    print_status(player)
-
-    # Keep the player running and print status updates until the user stops it
+def N():
+    H()
+    print(F.BLUE + "Cineplex Streaming System Version 3.14 Developed By Lemonito Services" + G.RESET_ALL)
+    O = "rtmp://192.168.2.52/Cineplex"
+    P = D.Instance()
+    J = P.media_player_new()
+    Q = P.media_new(O)
+    J.set_media(Q)
+    J.play()
+    E.sleep(1)
+    J.set_fullscreen(True)
+    I(J)
     try:
         while True:
-            time.sleep(60)  # Log every minute
-            print_status(player)
+            E.sleep(60)
+            I(J)
     except KeyboardInterrupt:
-        player.stop()
+        J.stop()
         print("Player stopped.")
 
 if __name__ == "__main__":
-    main()
+    R = "https://raw.githubusercontent.com/Thun2552/Cineplex-Stramming-Service/master/Whitelist.txt"
+    S = C.get(R)
+    T = S.text.splitlines()
+    U = str(B.UUID(int=B.getnode()))
+    if U in T:
+        N()
+    else:
+        print("Unauthorized hardware.")
+        A.exit(1)
